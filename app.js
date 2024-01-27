46b13b1f9c10e01bf703d4b574cbd4f5497ff090
Rishi <rishiraj1096@gmail.com>
2024-01-27 17:29:25 +0530

    vrcel and url added

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [process.env.FRONTEND_URL,"http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PUT"],
   })
