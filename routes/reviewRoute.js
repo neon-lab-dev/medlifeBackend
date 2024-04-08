2a5dbcb9d05ce1b3b26a09d129b4e04db97803fa
Rishi <rishiraj1096@gmail.com>
2024-04-08 15:33:43 +0530

    new update feaature for review

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { createReview, getAllReview } from "../controllers/reviewController.js";
+import { createReview, deleteReview, getAllAdminReview, getAllReview, updateReviewStatus } from "../controllers/reviewController.js";
+import {isAuthenticated} from "../middlewares/auth.js";
 
 const router = express.Router();
 
@@ -8,8 +9,16 @@ const router = express.Router();
 router.route("/createreview").post(createReview);
 
 
-//get All review---user/Admin
+//get All review---user
 router.route("/reviews").get(getAllReview);
+
+
+//get All Admin review
+router.route("/areviews").get(isAuthenticated,getAllAdminReview);
+
+//update review status
+router.route("/reviewstatus/:id").put(isAuthenticated,updateReviewStatus).delete(deleteReview);
+
 
 
 export default router;
